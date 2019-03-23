0b3e636e68834e7a22ea29b4ea71b9cde7c62cdd
Snarly Narwhal <pjlegendreiv@gmail.com>
2019-03-23 12:39:59 -0400

    Added support for MessagePack

---
+++ Tests/Networker.Tests.MessagePack/PingPacket.cs
@@ -0,0 +1,17 @@
+﻿using MessagePack;
+using System;
+
+namespace Networker.Tests.MessagePack 
+{
+	[MessagePackObject]
+    public class PingPacket
+    {
+		[Key(0)]
+		public virtual DateTime Time { get; set; }
+
+		public PingPacket(DateTime time) 
+		{
+			Time = time;
+		}
+    }
+}
